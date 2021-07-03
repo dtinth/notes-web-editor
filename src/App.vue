6ee26609bafbc52a240280a8165ee24b34d3bb4b
dtinth on MBP M1 <dtinth@spacet.me>
2021-07-03 17:36:11 +0700

    Add status bar

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {},
-});
+})
 </script>
 
 <style>
-@import "./App.css";
+@import './App.css';
 </style>
